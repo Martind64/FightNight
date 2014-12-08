5858bb81c643bada7888ceaf8c13bc63d4b095cd
Martind64 <Martind_64@hotmail.com>
2014-12-09 00:50:20 +0100

    Added IFighter Interface

---
+++ FightNight/IFighter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightNight
+{
+     public interface IFighter
+     {
+         string UpperCut { get; }
+
+         string Jab { get; }
+         string SpecialMove { get; }
+         string RightHook { get; }
+         string LeftHook { get; }
+         string SuckerPunch { get; }
+
+         
+
+
+     }
+}
